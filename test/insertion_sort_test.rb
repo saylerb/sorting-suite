cf7da8743578f1799bc3abb0de67f707edcf7695
saylerb <sayler.b@gmail.com>
2016-04-02 21:53:41 -0600

    completed bubble sort

--- test/insertion_sort_test.rb
+++ test/insertion_sort_test.rb
@@ -1,24 +1,24 @@
 require "minitest/autorun"
 require "minitest/pride"
-require "../lib/bubble_sort"
+require "../lib/insertion_sort"
 
-class BubbleSortTest < Minitest::Test
+class InsertionSortTest < Minitest::Test
   
 
-  def test_that_bubble_class_created
-    sorter = BubbleSort.new
-    assert_equal sorter.class, BubbleSort
+  def test_that_insertion_class_created
+    sorter = InsertionSort.new
+    assert_equal sorter.class, InsertionSort
   end
   
-  def test_bubble_sort
-    sorter = BubbleSort.new
+  def test_insertion_sort
+    sorter = InsertionSort.new
     puts 'sorting: ["d", "b", "a", "c"]'
     assert_equal ["a", "b", "c", "d"], sorter.sort(["d", "b", "a", "c"])
   end
 
   def test_that_it_can_sort_generated_arrays
     
-    sorter = BubbleSort.new
+    sorter = InsertionSort.new
     array = ("a".."z").to_a.shuffle
     assert_equal ("a".."z").to_a, sorter.sort(array)
   end
