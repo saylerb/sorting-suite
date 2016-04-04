84897016f6a97d95e6c432b5e560f2f2a5d05dae
saylerb <sayler.b@gmail.com>
2016-04-03 21:47:41 -0600

    adding more insertion sort tests

--- lib/insertion_sort.rb
+++ lib/insertion_sort.rb
@@ -2,32 +2,23 @@ class InsertionSort
 
   def sort(array)
     
-    unsorted_array = array.map { |e| e.dup } 
+    unsorted_array = array.map { |element| element } 
     new_array = []
-    iterator = 0
 
     while new_array.length < array.length
-      iterator += 1
-      puts "Outer Loop #{iterator}"
       insert = false
 
       if new_array.length == 0
-        puts "Creating new array!"
         new_array.unshift(unsorted_array.shift) 
-        puts "new_array: #{new_array}"
       end
       new_array.each_index do |index|
         if unsorted_array[0] < new_array[index]
-          puts "Adding to array..."
           new_array.insert(index, unsorted_array.shift)
-          puts "new_array: #{new_array}"
-          puts "unsorted: #{unsorted_array}"
           insert = true 
           break
         end
       end
       if not insert
-        puts "appending..."
         new_array.push(unsorted_array.shift)
       end
     end
@@ -36,12 +27,4 @@ class InsertionSort
 
 end
 
-sorter = InsertionSort.new
-puts 'sorting: ["d", "b", "a", "c"]'
-sorter.sort(["d", "b", "a", "c"]) 
-
-sorter2 = InsertionSort.new
-array = ("a".."z").to_a.shuffle
-puts "sorting #{array}"
-sorter2.sort(array)
  
