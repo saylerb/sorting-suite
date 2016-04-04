ae808d62f78a2de6d419fdb85823b5dba184758a
saylerb <sayler.b@gmail.com>
2016-04-04 11:20:48 -0600

    starting working on benchmarking

--- lib/benchmark.rb
+++ lib/benchmark.rb
@@ -3,23 +3,17 @@ require_relative "insertion_sort"
 require_relative  "merge_sort"
 require "benchmark"
 
-module SortingSuite
-  class Benchmark
-
-    def time(algorithm, array)
-      start_time = Time.now
-      sorter = algorithm.new(array)
-      end_time = Time.now
-      "Time elapsed #{calculate_time(start_time, stop_time)}"
-    end
+module SortingSuite 
 
 
-    def calculate_time(start, stop)
-      time_elapsed = stop - start
-      minutes = time_elapsed.to_i / 60
-      seconds = time_elapsed.to_i % 60
-      "#{minutes} minutes, #{seconds} seconds"
-    end
+  class Bench 
+
+    puts Benchmark.measure{SortingSuite::BubbleSort.new.sort(["d","c","b","a"])}
+    puts Benchmark.measure{SortingSuite::InsertionSort.new.sort(["d","c","b","a"])}
+    puts Benchmark.measure{SortingSuite::MergeSort.new.sort(["d","c","b","a"])}
+
   end
 end
+
+
 
